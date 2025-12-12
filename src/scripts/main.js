47a61af0ac60f7819c5a5fdc29596576aeeb46d8
Ivan <igrytsaenko@gmail.com>
2025-12-12 17:25:38 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,32 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  const spider = document.querySelector('.spider');
+  e.preventDefault();
+
   const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
 
-  const spiderSize = spider.getBoundingClientRect();
-  const wallSize = wall.getBoundingClientRect();
+  const wallRect = wall.getBoundingClientRect();
 
-  const relativeX = e.clientX - wallSize.left;
-  const relativeY = e.clientY - wallSize.top;
-  const desiredLeft = relativeX - spiderSize.width / 2;
-  const desiredTop = relativeY - spiderSize.height / 2;
-  const maxLeft = wall.clientWidth - spiderSize.width;
-  const maxTop = wall.clientWidth - spiderSize.height;
+  if (
+    e.clientX < wallRect.left ||
+    e.clientX > wallRect.right ||
+    e.clientY < wallRect.top ||
+    e.clientY > wallRect.bottom
+  ) {
+    return;
+  }
+
+  const spiderRect = spider.getBoundingClientRect();
+
+  const relativeX = e.clientX - wall.clientLeft;
+  const relativeY = e.clientY - wall.clientTop;
+
+  const desiredLeft = relativeX - spiderRect.width / 2;
+  const desiredTop = relativeY - spiderRect.height / 2;
+
+  const maxLeft = wall.clientWidth - spider.offsetWidth;
+  const maxTop = wall.clientHeight - spider.offsetHeight;
 
   const newLeft = Math.max(0, Math.min(desiredLeft, maxLeft));
   const newTop = Math.max(0, Math.min(desiredTop, maxTop));
